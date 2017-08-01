8e6123c9d31d0400d23f5aa05216f259c668222a
Pavlo Shchur <pavloshchur@gmail.com>
2017-08-01 19:18:23 +0300

    29/07/2017

---
+++ src/main/java/com/testportal/IndexServlet.java
@@ -0,0 +1,23 @@
+package com.testportal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet")
+public class IndexServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("myKey", "my value");
+        request.getRequestDispatcher("/WEB-INF/pages/index.jsp").forward(request, response);
+
+
+    }
+}
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,17 +1,17 @@
 <%--
   Created by IntelliJ IDEA.
   User: User
-  Date: 29.07.2017
-  Time: 12:29
+  Date: 01.08.2017
+  Time: 19:12
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Welcome</title>
+    <title>Index</title>
 </head>
 <body>
-    <h1>welcome</h1>
-    <a href="index">To index.</a>
+        <h2>Index page.</h2>
+        ${myKey}
 </body>
 </html>
